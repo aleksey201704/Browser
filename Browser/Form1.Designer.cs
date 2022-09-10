dcc2dfa9d666255187e6436d60b029269f5fdcfe
SenX_eb85@itstepru.onmicrosoft.com <SenX_eb85@itstepru.onmicrosoft.com>
2022-09-10 11:13:03 +0500

    Add project files.

---
+++ Browser/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace Browser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.btnAddTab = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnHistory = new System.Windows.Forms.Button();
+            this.btnSearchSetting = new System.Windows.Forms.Button();
+            this.btnFavorites = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.RightPanel = new System.Windows.Forms.Panel();
+            this.lstboxHistory = new System.Windows.Forms.ListBox();
+            this.PanelFavority = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TopPanel.SuspendLayout();
+            this.MainPanel.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.RightPanel.SuspendLayout();
+            this.PanelFavority.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.Controls.Add(this.btnAddTab);
+            this.TopPanel.Controls.Add(this.comboBox1);
+            this.TopPanel.Controls.Add(this.button1);
+            this.TopPanel.Controls.Add(this.btnHistory);
+            this.TopPanel.Controls.Add(this.btnSearchSetting);
+            this.TopPanel.Controls.Add(this.btnFavorites);
+            this.TopPanel.Controls.Add(this.btnHome);
+            this.TopPanel.Controls.Add(this.textBox1);
+            this.TopPanel.Controls.Add(this.btnBack);
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(1113, 33);
+            this.TopPanel.TabIndex = 0;
+            // 
+            // btnAddTab
+            // 
+            this.btnAddTab.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnAddTab.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnAddTab.Location = new System.Drawing.Point(32, 0);
+            this.btnAddTab.Name = "btnAddTab";
+            this.btnAddTab.Size = new System.Drawing.Size(32, 33);
+            this.btnAddTab.TabIndex = 9;
+            this.btnAddTab.Text = "+";
+            this.btnAddTab.UseVisualStyleBackColor = true;
+            this.btnAddTab.Click += new System.EventHandler(this.btnAddTab_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Google",
+            "Яндекс",
+            "Mail",
+            "bing"});
+            this.comboBox1.Location = new System.Drawing.Point(646, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(187, 28);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(953, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(32, 33);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "🏠";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnHistory
+            // 
+            this.btnHistory.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnHistory.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnHistory.Location = new System.Drawing.Point(985, 0);
+            this.btnHistory.Name = "btnHistory";
+            this.btnHistory.Size = new System.Drawing.Size(32, 33);
+            this.btnHistory.TabIndex = 6;
+            this.btnHistory.Text = "📖";
+            this.btnHistory.UseVisualStyleBackColor = true;
+            this.btnHistory.Click += new System.EventHandler(this.btnHistory_Click);
+            // 
+            // btnSearchSetting
+            // 
+            this.btnSearchSetting.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnSearchSetting.Location = new System.Drawing.Point(1017, 0);
+            this.btnSearchSetting.Name = "btnSearchSetting";
+            this.btnSearchSetting.Size = new System.Drawing.Size(32, 33);
+            this.btnSearchSetting.TabIndex = 5;
+            this.btnSearchSetting.UseVisualStyleBackColor = true;
+            // 
+            // btnFavorites
+            // 
+            this.btnFavorites.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnFavorites.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnFavorites.Location = new System.Drawing.Point(1049, 0);
+            this.btnFavorites.Name = "btnFavorites";
+            this.btnFavorites.Size = new System.Drawing.Size(32, 33);
+            this.btnFavorites.TabIndex = 4;
+            this.btnFavorites.Text = "✨";
+            this.btnFavorites.UseVisualStyleBackColor = true;
+            this.btnFavorites.Click += new System.EventHandler(this.btnFavorites_Click);
+            // 
+            // btnHome
+            // 
+            this.btnHome.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnHome.Location = new System.Drawing.Point(1081, 0);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(32, 33);
+            this.btnHome.TabIndex = 3;
+            this.btnHome.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(139, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(502, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnBack.Location = new System.Drawing.Point(0, 0);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(32, 33);
+            this.btnBack.TabIndex = 0;
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Controls.Add(this.tabControl1);
+            this.MainPanel.Controls.Add(this.RightPanel);
+            this.MainPanel.Controls.Add(this.PanelFavority);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(0, 33);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(1113, 665);
+            this.MainPanel.TabIndex = 2;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 33);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(828, 632);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.webView21);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(820, 606);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Стартовая старница";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView21.Location = new System.Drawing.Point(0, 0);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(820, 606);
+            this.webView21.Source = new System.Uri("https://www.microsoft.com", System.UriKind.Absolute);
+            this.webView21.TabIndex = 0;
+            this.webView21.ZoomFactor = 1D;
+            this.webView21.NavigationCompleted += new System.EventHandler<Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs>(this.webView21_NavigationCompleted);
+            this.webView21.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // RightPanel
+            // 
+            this.RightPanel.Controls.Add(this.lstboxHistory);
+            this.RightPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.RightPanel.Location = new System.Drawing.Point(828, 33);
+            this.RightPanel.Name = "RightPanel";
+            this.RightPanel.Size = new System.Drawing.Size(285, 632);
+            this.RightPanel.TabIndex = 3;
+            this.RightPanel.Visible = false;
+            // 
+            // lstboxHistory
+            // 
+            this.lstboxHistory.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstboxHistory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lstboxHistory.FormattingEnabled = true;
+            this.lstboxHistory.ItemHeight = 20;
+            this.lstboxHistory.Location = new System.Drawing.Point(0, 0);
+            this.lstboxHistory.Name = "lstboxHistory";
+            this.lstboxHistory.Size = new System.Drawing.Size(285, 632);
+            this.lstboxHistory.TabIndex = 0;
+            // 
+            // PanelFavority
+            // 
+            this.PanelFavority.Controls.Add(this.label1);
+            this.PanelFavority.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PanelFavority.Location = new System.Drawing.Point(0, 0);
+            this.PanelFavority.Name = "PanelFavority";
+            this.PanelFavority.Size = new System.Drawing.Size(1113, 33);
+            this.PanelFavority.TabIndex = 0;
+            this.PanelFavority.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Silver;
+            this.label1.Location = new System.Drawing.Point(703, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1113, 698);
+            this.Controls.Add(this.MainPanel);
+            this.Controls.Add(this.TopPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.TopPanel.ResumeLayout(false);
+            this.TopPanel.PerformLayout();
+            this.MainPanel.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.RightPanel.ResumeLayout(false);
+            this.PanelFavority.ResumeLayout(false);
+            this.PanelFavority.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel TopPanel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.Button btnFavorites;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnHistory;
+        private System.Windows.Forms.Button btnSearchSetting;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel RightPanel;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.Panel PanelFavority;
+        private System.Windows.Forms.ListBox lstboxHistory;
+        private System.Windows.Forms.Button btnAddTab;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Browser/Form1.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.Web.WebView2.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Browser
+{
+    public partial class Form1 : Form
+    {
+       // public int[] countLabel { get; set; }
+        public int widhtcount { get; set; } = 0;
+        public Dictionary<string,string> factorymain { get; set; } = new Dictionary<string, string>(); 
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Первоначальные настройки
+            Uri uri = new Uri("https://www.microsoft.com");
+            webView21.Source = uri;
+            comboBox1.SelectedIndex = 0;
+
+
+        }
+
+
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+
+            if (e.KeyChar == 13 && comboBox1.SelectedIndex == 0 && textBox1.Text.Substring(0, 7) != "http://")
+            {
+                Uri urigoogle = new Uri($"https://www.google.com/search?q={textBox1.Text}");
+                webView21.Source = urigoogle;
+            }
+            if (textBox1.Text.Length > 7 && e.KeyChar == 13)
+            {
+                if (textBox1.Text.Substring(0, 7) == "http://")
+                {
+
+                    Uri urinew = new Uri(textBox1.Text);
+                    webView21.Source = urinew;
+                }
+            }
+
+            if (e.KeyChar == 13 && comboBox1.SelectedIndex == 1)
+            {
+                Uri uriya = new Uri($"https://yandex.ru/search/?text={textBox1.Text}");
+                webView21.Source = uriya;
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Uri uri = new Uri("https://www.microsoft.com");
+            webView21.Source = uri;
+        }
+
+        private void btnHistory_Click(object sender, EventArgs e)
+        {
+            if (RightPanel.Visible) { RightPanel.Visible = false; return; }
+            RightPanel.Visible = true;
+            
+
+            //MessageBox.Show(webView21.CoreWebView2.navi ToString());
+
+        }
+
+        
+        private void btnFavorites_Click(object sender, EventArgs e)
+        {
+
+            PanelFavority.Visible = true;
+            Label countLabel = new Label();
+            countLabel.Name = $"countLabel{widhtcount}";// widhtcount.ToString();
+            factorymain.Add(countLabel.Name, webView21.Source.ToString());
+            widhtcount++;
+            countLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            countLabel.Dock = System.Windows.Forms.DockStyle.Left;
+            countLabel.Height = 32;
+            countLabel.Width = 150;
+            countLabel.BackColor = Color.WhiteSmoke;
+
+            countLabel.Text = webView21.CoreWebView2.DocumentTitle;
+            //countLabel.MouseMove +=this.CountLabel_MouseMove;
+            countLabel.MouseMove += CountLabel_MouseMove;
+            countLabel.MouseClick += CountLabel_MouseClick;
+            countLabel.MouseLeave += CountLabel_MouseLeave;
+            this.PanelFavority.Controls.Add(countLabel);
+            
+
+        }
+
+        private void CountLabel_MouseMove(object sender, MouseEventArgs e)
+        {
+            Label countLabel = new Label();
+            countLabel=sender as Label;
+            countLabel.BackColor = Color.Silver;
+            //countLabel.MouseLeave += CountLabel_MouseLeave;
+           
+            
+        }
+
+        private void CountLabel_MouseClick(object sender, MouseEventArgs e)
+        {
+            Label countLabel = new Label();
+            countLabel = sender as Label;
+            //MessageBox.Show(countLabel.Name);
+
+            foreach (var item in factorymain)
+            {
+                if (countLabel.Name == item.Key)
+                {
+                    Uri uri = new Uri(item.Value);
+                    webView21.Source = uri;
+                }
+            }
+        }
+
+        private void CountLabel_MouseLeave(object sender, EventArgs e)
+        {
+            Label countLabel = new Label();
+            countLabel = sender as Label;
+            countLabel.BackColor = Color.WhiteSmoke;
+        }
+
+        private void webView21_NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e)
+        {
+
+            lstboxHistory.Items.Add(webView21.Source.ToString());
+
+        }
+
+        private void btnAddTab_Click(object sender, EventArgs e)
+        {
+            tabControl1.TabPages.Add("Новая вкладка");
+            WebView2 webview22 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.tabControl1.TabPages[1].Controls.Add(webview22);
+            webview22.Dock = DockStyle.Fill;
+            Uri uri = new Uri("https://www.mail.ru");
+            webview22.Source = uri;
+        }
+
+      
+    }
+}
